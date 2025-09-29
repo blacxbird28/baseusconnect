2c5444db08aadaaef9bbc2fa5cf2f95ae5d4f481
Sajarotul Yakin <yakinsajarotul@gmail.com>
2025-09-29 14:40:58 +0700

    update

---
+++ bootstrap/app.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+        // $middleware->group('web', [
+        //     \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        //     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        //     \Illuminate\Session\Middleware\StartSession::class,
+        //     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        //     \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        //     \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            // \Spatie\Permission\Middlewares\RoleMiddleware::class,
+            // \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+            // \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        // ]);
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
